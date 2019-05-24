35e829bfd136f8fb14f24afccfe5c73cd9b11f23
wondervictor <victorchanchina@gmail.com>
2019-05-24 12:25:54 +0800

    fix bugs

--- configs/hrnet/faster_rcnn_hrnetv2p_w18_syncbn_16batch_mstrain_nearscales_1x.py
+++ configs/hrnet/faster_rcnn_hrnetv2p_w18_syncbn_16batch_mstrain_nearscales_1x.py
@@ -180,7 +180,7 @@ log_config = dict(
 total_epochs = 12
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/faster_rcnn_hrnetv2p_w18_mstrain_sync_16batch_1x'
+work_dir = './work_dirs/faster_rcnn_hrnetv2p_w18_mstrain_sync_16batch_nearscales_1x'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
